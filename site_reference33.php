713a7f6526a5654930d935b41f5a4b604da612c1
korn <tanakorn18@hotmail.com>
2012-10-29 09:27:13 +0700

    test

---
+++ site_reference33.php
@@ -0,0 +1,202 @@
+<?session_start();?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+	<title>Innovation Matching</title>
+	<meta name="Generator" content="EditPlus">
+	<meta name="Author" content="">
+	<meta name="Keywords" content="">
+	<meta name="Description" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+    <meta name="robots" content="all" />
+    <meta name="robots" content="index, follow" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="version" content="1.0" />
+    <meta name="copyright" content="" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    
+    <!-- This file is part of a site template for sale at ThemeForest.net.
+         See: http://themeforest.net/user/ddstudios/portfolio
+         Copyright 2009 Dany Duchaine <ddwebstudios@hotmail.com> -->
+    
+    <!-- CSS-->
+    <link href="../css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+	<link href="../css/thickbox.css" rel="stylesheet" type="text/css" />
+    <!-- //CSS -->
+	
+	<!-- Javascript-->
+	<script type='text/javascript' src='../javascript/jquery-1.2.3.min.js'></script>
+	<script type='text/javascript' src='../javascript/kriesi.js'></script>
+	<script type='text/javascript' src='../javascript/cufon-yui.js'></script>
+	<script type='text/javascript' src='../javascript/nevis_700.font.js'></script>
+	<script type="text/javascript" src="../javascript/jquery.innerfade.js"></script>
+	
+	<script type="text/javascript">
+	   $(document).ready(
+				function(){
+					
+					$('#left_content').innerfade({
+					animationtype: 'slide', 
+						speed: 'slow',
+						timeout: 4000,
+						type: 'random',
+						containerheight: '150px'
+					});
+
+			});
+  	</script>
+
+	
+	<script type="text/javascript">
+    
+        Cufon.replace('h1, h2, .blog, .footer', { hover: 'true' });
+    
+    </script>
+	<!-- Javascript-->
+
+</head>
+<!-- // head -->
+
+<!-- Body -->
+<body>
+
+		<?include "../manu.php";?>
+
+		<!-- Top text -->
+	<div id="text_wrap">
+		<div id="top_text_sub">	
+		<div class="title">
+			<h1>site reference</h1>
+		</div>
+		<div class="description">
+			<b><p>ไซท์งานในพื้นที่ต่างๆ</p></b>
+		</div> 
+		</div>
+	</div>
+		<!-- // Top text -->
+		
+		<!-- Content wrap -->
+		<div id="content_wrap">
+		
+		<!-- Top content -->
+		<div id="top_content">
+		
+		<!-- Left content -->
+		<div id="left_content_about_blog">
+			<img src="../images/baner6.gif" width="200"/><br><br>
+			<h3><b>บริษัท เอ็มเค เรสโตรองต์ กรุ๊ป จำกัด(มหาชน)</b></h3>
+			<h2></h2>
+		<center><p><table width=500>
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/1.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/2.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/3.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/4.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/5.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/6.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/7.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/8.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/9.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/10.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/11.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+			<tr><td width=500 align="center">
+				<img class="portfolio_img" alt="big_img_1" src="images/site11/12.jpg" width="350"/><br><br>
+				<h3></h3>
+				<h2></h2><br><br>
+			</td></tr>
+
+		</table></p></center>	
+				
+		
+		</div>
+		<!-- // Left content -->
+		
+		<!-- Sidebar -->
+
+			<div id="sidebar">
+			<img class="bubble" src="images/1.png" alt="fromtheblog" />
+			<div class="post">
+			<?	$idtep = $_GET['idtep'];?>
+			<?include "newsite.php";?>
+			</div>
+			<a class="blogbtn" href="site_reference.php?idtep=<?=$idtep;?>"><< BACK</a>
+		</div>
+		<!-- //Sidebar -->
+		
+		</div>
+		<!-- // Top content -->
+		
+	</div>
+		<!-- // Content wrap -->
+		
+		<!-- Bottom content -->
+			
+</body>
+<!-- // Body -->
+
+</html>
